37530c33a5b110fd620f83a685711cf6656de1ae
rasisa64 <rasisa64@hotmail.com>
2022-07-10 14:54:49 -0500

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    loadChildren: () => import('./features/formulario/formulario.module').then(m => m.FormularioModule)
+  },
+  {
+    path: 'tabla',
+    loadChildren: () => import('./features/tabla/tabla.module').then(m => m.TablaModule)
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/shared/stores/contact/contact.state.ts
@@ -0,0 +1,6 @@
+export interface ContactState {
+    name: string;
+    email: string;
+    phone: string;
+  }
+  
