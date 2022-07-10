c042fa643453783b5f5af3f3218f9a782fdd474e
Alejandro Rodriguez Romero <alex98rodriguezromero@gmail.com>
2022-07-10 14:59:37 -0500

    revisado

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ JosePinedo
 
 - Adicional 1
 - Adicional 2
+
+
+## Resultado
+
+Observaciones:
+
+- Para el desarrollo de un proyecto Angular lo mejor podria ser pensarlo como si fuera un rompecabezas, en donde de poco vas armando el total de la aplicacion.
+- Si bien hay logica al ir agregando la funcionalidad, me parece que se debio ir adaptando de los ejemplos poco segun los pasos del examen.
+- Mi recomendacion, intentar probar nuevamente este proyecto, sin copiar y pegar mejor seria transcribir asi se tiene un mejor control de lo que se va desarrollando.
+
+Saludos
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './shared/pages/not-found/not-found.component';
+import { NgxsModule } from '@ngxs/store';
+import { PersonNgxsState } from './shared/stores/person-ngxs/person-ngxs.state';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,13 @@ import { NotFoundComponent } from './shared/pages/not-found/not-found.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxsModule.forRoot(
+      [PersonNgxsState],
+      {
+        developmentMode: !environment.production
+      }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
